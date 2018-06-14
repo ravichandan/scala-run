f7a0bbc29284214240ef0de99c4973a43bc14e01
ravi.sithiraju <ravi.sithiraju@healthdirect.org.au>
2018-06-14 11:54:17 +1000

    bomber man game final

--- src/com/sample/test/TheBomberManGame.scala
+++ src/com/sample/test/TheBomberManGame.scala
@@ -5,45 +5,47 @@ object TheBomberManGame {
   // Complete the bomberMan function below.
   def bomberMan(n: Int, grid: Array[String]): Array[String] = {
 
-
+    if (n == 1) return grid;
     val grid2: Array[Array[Int]] = grid.map(line => line.map(c => if (c == 'O') 2 else 0).toArray);
-    for (i <- 0 until grid2.length) {
-      for (j <- 0 until grid2(i).length) {
-        print(grid2(i)(j));
-      }
-      println();
-    }
+    //    val pr = (grid: Array[Array[Int]]) => grid.map(line => {
+    //      line.map(n => print(n)); println();
+    //    });
 
-    println();
-    println();
-    //    for (c <- 0 until itr) {
-    for (_ <- 0 until n % 4) {
+    for (_ <- 0 until (n % 4) + 3) {
       for (i <- 0 until grid2.length) {
         for (j <- 0 until grid2(i).length) {
-          if (grid2(i)(j) == 0) {
+          if (grid2(i)(j) == -1) {
+            grid2(i)(j) = 0;
+          }
+          else if (grid2(i)(j) == 0) {
             grid2(i)(j) = 3;
           } else {
             if (grid2(i)(j) == 1) {
-              if (i > 0 && grid2(i - 1)(j) != 1) {
+              if (i > 0 && grid2(i - 1)(j) != 0) {
                 grid2(i - 1)(j) = 0;
               }
-              if (j > 0 && grid2(i)(j - 1) != 1) {
+              if (j > 0 && grid2(i)(j - 1) != 0) {
                 grid2(i)(j - 1) = 0;
               }
               if (i < grid2.length - 1 && grid2(i + 1)(j) != 1) {
-                grid2(i + 1)(j) = 1;
+                grid2(i + 1)(j) = -1;
               }
               if (j < grid2(i).length - 1 && grid2(i)(j + 1) != 1) {
-                grid2(i)(j + 1) = 1;
+                grid2(i)(j + 1) = -1;
               }
             }
             grid2(i)(j) -= 1;
           }
-          print(grid2(i)(j));
+
         }
-        println();
+
       }
-      println("--------------");
+
+
+      //      pr(grid2);
+
+
+      //      println("--------------");
     }
     val chars: Array[String] = grid2.map(arr => new String(arr.map(i => if (i == 0) '.' else 'O')));
 
@@ -65,10 +67,10 @@ object TheBomberManGame {
     //    print((1 / 3));
     //    }
     val grid2: Array[String] = Array(
-      "...",
-      ".O.",
-      "...")
-    bomberMan(3, grid).map(ele => println(ele));
+      "O.O.",
+      ".O.O",
+      "O.O.")
+    bomberMan(5, grid2).map(ele => println(ele));
     //    bomberMan(3, grid2).map(ele => println(ele));
   }
 }
